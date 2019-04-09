d3cb37854357ae1c9d326e28eb8ad214e86fe14c
simplex991 <stefanh.arnason@outlook.com>
2019-04-09 19:59:08 +0200

    first scripts

---
+++ CubeRotation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotation : MonoBehaviour
+{
+    public Transform sphereTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sphereTransform.parent = transform; // Parents sphere to cube
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotating objects
+        //transform.eulerAngles += Vector3.up * 180 * Time.deltaTime; // Vector.up => Vector3(0, 1, 0)
+        transform.Rotate(Vector3.up * 180 * Time.deltaTime, Space.World); // Space.World = global space | Space.Self = object space
+        // eulerAngles makes the child object rotate around the parents global space
+        // Rotate makes the child object rotate around the parents object space
+    }
+}
---
+++ TimeGame.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeGame : MonoBehaviour
+{
+    float roundStartDelayTime = 3;
+    float roundStartTime;
+    int waitTime;
+    bool roundStarted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Press the spacebar once you think the allotted time is up.");
+        Invoke("SetNewRandomTime", roundStartDelayTime); // Invoke sets a delay (function, time)
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.Space) && roundStarted){
+            InputReceived();
+        }
+    }
+
+    void InputReceived()
+    {
+        roundStarted = false;
+        float playerWaitTime = Time.time - roundStartTime;
+        float error = Mathf.Abs(waitTime - playerWaitTime);
+
+        print("Error you waited for " + playerWaitTime + " seconds. That's " + error + " seconds off");
+        SetNewRandomTime();
+    }
+
+    void SetNewRandomTime()
+    {
+        waitTime = Random.Range(5, 21);
+        roundStartTime = Time.time;
+        roundStarted = true;
+
+        print(waitTime + " seconds.");
+    }
+}
