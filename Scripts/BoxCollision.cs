75260344319947d7d3548d5f5ff209a369dc0eb4
simplex991 <stefanh.arnason@outlook.com>
2019-04-10 00:42:38 +0200

    added a box collision script

---
+++ Scripts/BoxCollision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxCollision : MonoBehaviour
+{
+    Rigidbody myRigidbody;
+    Vector3 velocity;
+    public float speed = 6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 direction = input.normalized;
+        velocity = direction * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        myRigidbody.position += velocity * Time.fixedDeltaTime;
+    }
+}
